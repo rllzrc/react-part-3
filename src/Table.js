08d46e15eabd13030833a15af3e894e1a680b32a
Rella <rellas.email@gmail.com>
2020-03-25 14:49:13 -0400

    add table component and setup initial files

---
+++ src/Table.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Best Bud</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Mascot</td>
+      </tr>
+    </tbody>
+  )
+}
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    )
+  }
+}
+
+export default Table;
